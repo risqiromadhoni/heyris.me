dacd61cbae05cfb99d50b46347d30b624f33f2fa
Risqi Romadhoni <risqi.romadhoni96@gmail.com>
2024-09-24 10:09:27 +0700

    chore: page complete

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,6 +19,11 @@ export const ui = {
 		"menu.resource": "Resource",
 		"post.toc": "Table of Contents",
 		"common.viewAll": "View all",
+		"page.about.title": "About Me",
+		"page.about.description":
+			"I am a software engineer with experience ±7 who loves to learn and share knowledge.",
+		"page.about.aboutMe":
+			"Hi, I’m a software engineer with over 7 years of experience. Loving gardening in hydroponics 🪴 and aquaponics 🪷 when I'm not coding 🧑🏻‍💻.",
 	},
 	id: {
 		"social.findMe": "Temukan saya di",
@@ -28,5 +33,10 @@ export const ui = {
 		"menu.resource": "Sumber",
 		"post.toc": "Daftar Isi",
 		"common.viewAll": "Lihat semua",
+		"page.about.title": "Tentang Saya",
+		"page.about.description":
+			"Saya adalah seorang insinyur perangkat lunak dengan pengalaman ±7 yang senang belajar dan berbagi pengetahuan.",
+		"page.about.aboutMe":
+			"Hai, saya seorang insinyur perangkat lunak dengan pengalaman lebih dari 7 tahun. Menyukai berkebun dalam hidroponik 🪴 dan akuaponik 🪷 ketika saya tidak sedang melakukan coding 🧑🏻‍💻.",
 	},
 } as const;
